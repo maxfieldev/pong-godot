98d9c3cd40c3fa4a4955e8371250eb867ba420cc
maxfieldev <maxfield.dev@gmail.com>
2024-12-22 18:42:47 -0800

    End of part 5

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var player1: Paddle = $Player1
 @onready var player2: Paddle = $Player2
 @onready var ball: Ball = $Ball
+@onready var ui: UI = $CanvasLayer/UI
 
 const PADDLE_SPACING_FROM_EDGE: float = 100
 const SERVE_TIME: float = 1.5
@@ -14,13 +15,14 @@ var screen_size: Vector2
 var game_state = GameStates.START
 
 enum GameStates {GAME, SERVE, GAME_OVER, START}
-enum Player {PLAYER1, PLAYER2}
+enum Player {PLAYER1 = 0, PLAYER2 = 1}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	player1.position = Vector2(PADDLE_SPACING_FROM_EDGE, screen_size.y/2)
 	player2.position = Vector2(screen_size.x-PADDLE_SPACING_FROM_EDGE, screen_size.y/2)
+	ball.visible = false
 	ball.reset()
 	
 	ball.scored.connect(_on_score)
@@ -39,20 +41,22 @@ func _physics_process(delta: float) -> void:
 		get_tree().reload_current_scene()
 	elif game_state == GameStates.START && Input.is_action_just_pressed("start"):
 		_serve_ball()
+		ball.visible = true
+		ui.on_start_game()
 
 func _on_score() -> void:
 	if _player1_scored():
 		player1_score += 1
+		ui.update_score(Player.PLAYER1, player1_score)
 		if _winning_score(player1_score):
 			_set_game_over(Player.PLAYER1)
 			return
 	else:
 		player2_score += 1
+		ui.update_score(Player.PLAYER2, player2_score)
 		if _winning_score(player2_score):
 			_set_game_over(Player.PLAYER2)
 			return
-	print("Player 1 Score: " + str(player1_score))
-	print("Player 2 Score: " + str(player2_score))
 	ball.reset()
 	game_state = GameStates.SERVE
 	get_tree().create_timer(SERVE_TIME).timeout.connect(_serve_ball)
@@ -68,7 +72,7 @@ func _winning_score(score: int) -> bool:
 
 func _set_game_over(winner: Player) -> void:
 	if winner == Player.PLAYER1:
-		print("PLAYER 1 WINS!!!!")
+		ui.on_game_over("Player 1 WINS!!")
 	else:
-		print("PLAYER 2 WINS!!!!")
+		ui.on_game_over("Player 2 WINS!!")
 	game_state = GameStates.GAME_OVER
