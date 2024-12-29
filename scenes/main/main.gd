a6c34aa89db2354cdf8e4e1b2cf5e5697b544646
maxfieldev <maxfield.dev@gmail.com>
2024-12-28 17:36:33 -0800

    End of part 6

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -4,15 +4,23 @@ extends Node2D
 @onready var player2: Paddle = $Player2
 @onready var ball: Ball = $Ball
 @onready var ui: UI = $CanvasLayer/UI
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 const PADDLE_SPACING_FROM_EDGE: float = 100
 const SERVE_TIME: float = 1.5
-const WINNING_THRESHOLD: int = 1
+const WINNING_THRESHOLD: int = 10
+const NETTING_COLOR: Color = Color(0.94, 0.96, 0.94, 1.0)
+const NETTING_WIDTH: float = 6
+const NETTING_DASH: float = 25
 
 var player1_score: int = 0
 var player2_score: int = 0
 var screen_size: Vector2
 var game_state = GameStates.START
+var sounds: Dictionary = {
+	"score": preload("res://scenes/main/pong_score.wav"),
+	"win": preload("res://scenes/main/pong_win.wav")
+}
 
 enum GameStates {GAME, SERVE, GAME_OVER, START}
 enum Player {PLAYER1 = 0, PLAYER2 = 1}
@@ -26,6 +34,7 @@ func _ready() -> void:
 	ball.reset()
 	
 	ball.scored.connect(_on_score)
+	audio_player.stream = sounds["score"]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -43,6 +52,11 @@ func _physics_process(delta: float) -> void:
 		_serve_ball()
 		ball.visible = true
 		ui.on_start_game()
+		queue_redraw()
+
+func _draw() -> void:
+	if game_state == GameStates.GAME:
+		draw_table_netting()
 
 func _on_score() -> void:
 	if _player1_scored():
@@ -58,6 +72,7 @@ func _on_score() -> void:
 			_set_game_over(Player.PLAYER2)
 			return
 	ball.reset()
+	audio_player.play()
 	game_state = GameStates.SERVE
 	get_tree().create_timer(SERVE_TIME).timeout.connect(_serve_ball)
 	
@@ -76,3 +91,11 @@ func _set_game_over(winner: Player) -> void:
 	else:
 		ui.on_game_over("Player 2 WINS!!")
 	game_state = GameStates.GAME_OVER
+	audio_player.stream = sounds["win"]
+	audio_player.play()
+	queue_redraw()
+
+func draw_table_netting() -> void:
+	var top_middle_of_screen: Vector2 = Vector2(screen_size.x/2, 0)
+	var btm_middle_of_screen: Vector2 = Vector2(screen_size.x/2, screen_size.y)
+	draw_dashed_line(top_middle_of_screen, btm_middle_of_screen, NETTING_COLOR, NETTING_WIDTH, NETTING_DASH)
